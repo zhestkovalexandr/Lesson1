ddb4fc6bfd618237bbdb0d58cf8bfc47f14d23f6
Alex <zhestkov.82@mail.ru>
2022-10-01 22:04:17 +0400

    task4 A=2 B=3 C=7

--- Lesson1/task4/Program.cs
+++ Lesson1/task4/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 2;
+int numberB = 3;
+int numberC = 7;
+int max = numberA;
+if (numberA>max) max = numberA;
+if (numberB>max) max = numberB;
+if (numberC>max) max = numberC;
+Console.Write("Макс = ");
+Console.WriteLine(max);
